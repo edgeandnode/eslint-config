938f3b815a66afa7df5f7cbb9d5b6f70e6fea3dc
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2023-07-31 15:04:50 +0200

    Move all TypeScript ESLint rules to one file

--- src/rules/typescriptEslintRules.ts
+++ src/rules/typescriptEslintRules.ts
@@ -1,5 +1,7 @@
 import type { Linter } from "eslint";
 
+import { mapValues } from "../util/mapValues";
+
 export const typescriptEslintRules: Linter.RulesRecord = {
   // based on `@typescript-eslint/recommended` config
   "@typescript-eslint/adjacent-overload-signatures": "error",
@@ -154,6 +156,74 @@ export const typescriptEslintRules: Linter.RulesRecord = {
 
   // we never used tslint
   "@typescript-eslint/ban-tslint-comment": "off",
+};
+
+const _typecheckedRules: Linter.RulesRecord = {
+  // Needs `project` specified in parserOptions
+  "@typescript-eslint/await-thenable": "warn",
+  "@typescript-eslint/no-floating-promises": "warn",
+  "@typescript-eslint/no-for-in-array": "error",
+  "@typescript-eslint/no-implied-eval": "error",
+  "@typescript-eslint/no-misused-promises": "error",
+  /**
+   * @changed from error to warn
+   * @motivation It's a redundancy, but not a bug.
+   */
+  "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+
+  /**
+   * @changed
+   * @motivation Those two can be ran on CI, but ESLint language server doesn't
+   * play with them well.
+   */
+  "@typescript-eslint/no-unsafe-assignment": "off",
+  "@typescript-eslint/no-unsafe-call": "off",
+
+  /**
+   * @changed
+   * @motivation False positives on enums generated from GraphQL.
+   */
+  "@typescript-eslint/no-unsafe-member-access": "off",
+  "@typescript-eslint/no-unsafe-return": "warn",
+  "@typescript-eslint/prefer-regexp-exec": "warn",
+  "@typescript-eslint/require-await": "off",
+  /**
+   * @changed
+   * @motivation Implicit string coercion is harmless in our usecases.
+   */
+  "@typescript-eslint/restrict-plus-operands": "off",
+  /**
+   * @changed
+   * @motivation Blocked by Theme UI strict types.
+   */
+  "@typescript-eslint/restrict-template-expressions": "off",
+  /**
+   * @changed
+   * @motivation Hard to detect. A lot of false positives.
+   */
+  "@typescript-eslint/unbound-method": "off",
+  "@typescript-eslint/no-use-before-define": [
+    "warn",
+    {
+      functions: false,
+      classes: false,
+      variables: false,
+      typedefs: false,
+    },
+  ],
+  "@typescript-eslint/no-useless-constructor": "warn",
+
+  // #region "@typescript-eslint/strict"
+  // https://typescript-eslint.io/rules/
+
+  "@typescript-eslint/no-base-to-string": "warn",
+  "@typescript-eslint/no-unnecessary-boolean-literal-compare": "warn",
+  "@typescript-eslint/no-unnecessary-condition": "warn",
+  "@typescript-eslint/no-unnecessary-type-arguments": "warn",
+  "@typescript-eslint/non-nullable-type-assertion-style": "warn",
+  "@typescript-eslint/prefer-includes": "warn",
+  "@typescript-eslint/prefer-return-this-type": "warn",
+  "@typescript-eslint/prefer-string-starts-ends-with": "warn",
 
   "@typescript-eslint/no-confusing-void-expression": "off",
 
@@ -177,4 +247,15 @@ export const typescriptEslintRules: Linter.RulesRecord = {
   "@typescript-eslint/prefer-reduce-type-parameter": ["warn"],
   "@typescript-eslint/prefer-ts-expect-error": ["warn"],
   "@typescript-eslint/unified-signatures": ["warn"],
+
+  // #endregion
+};
+
+/**
+ * TypeScript ESLint rules requiring type checking provide useful semantic cannot be used if ESLint is
+ * used without type checking.
+ */
+export const typecheckedRules = {
+  on: _typecheckedRules,
+  off: mapValues(_typecheckedRules, () => "off" as const),
 };
